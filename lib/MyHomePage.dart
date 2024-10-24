14b252fc9741f046d3673e6672bc2739b71e9531
Maysara <maysaracs1001@gmail.com>
2024-10-24 16:03:05 +0800

    scanning result work ok

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -40,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 const SizedBox(height: 20),
                 Center(
-                  child: ElevatedButton(onPressed: ()=>controller.scanDevicesTwo(),
+                  child: ElevatedButton(onPressed: ()=>controller.scanDevices(),
                       style:ElevatedButton.styleFrom(
                           foregroundColor: Colors.white,
                           backgroundColor: Colors.blue,
@@ -52,23 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: const Text("Scan",
                         style: TextStyle(fontSize:18 ),
                       )),
-
-                ),
-                const SizedBox(height: 20),
-                Center(
-                  child: ElevatedButton(onPressed: ()=>controller.DisplayResult(),
-                      style:ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.blue,
-                          minimumSize: const Size(350, 55),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(5)),
-                          )
-                      ),
-                      child: const Text("display result",
-                        style: TextStyle(fontSize:18 ),
-                      )),
-
                 ),
                 const SizedBox(height: 20),
                 StreamBuilder<List<ScanResult>>(
@@ -84,9 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               return Card(
                                 elevation: 2,
                                 child: ListTile(
-                                  //title: Text(data.device.name),
-                                  title: Text(data.device.platformName.toString()),
-                                  // subtitle: Text(data.device.id.id),
+                                  title: Text(data.device.platformName),
                                   subtitle: Text(data.device.remoteId.toString()),
                                   trailing: Text(data.rssi.toString()),
                                 ),
--- lib/ble_controller.dart
+++ lib/ble_controller.dart
@@ -3,32 +3,14 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class BleController extends GetxController{
-
   final List<ScanResult> _scanResults = [];
   final Map<String, bool> _connectionStatus = {};
   var _observedScanResults = <ScanResult>[].obs;
 
-  // Future scanDevices() async{
-  //   if(await Permission.bluetoothScan.request().isGranted){
-  //       FlutterBluePlus.startScan(timeout: const Duration(seconds: 60));
-  //       Future.delayed(const Duration(seconds: 60), () {
-  //         FlutterBluePlus.scanResults.listen((results) {
-  //           print(results);
-  //           _observedScanResults.assignAll(results);
-  //         });
-  //         FlutterBluePlus.stopScan();
-  //       });
-  //       // print("hello world");
-  //       //FlutterBluePlus.stopScan();
-  //     }
-  //
-  //     // Future.delayed(const Duration(seconds: 60), () {
-  //     //   FlutterBluePlus.stopScan();
-  //     // });
-  //   }
-  Future scanDevicesTwo() async{
-    if(await Permission.bluetoothScan.request().isGranted){
+  FlutterBluePlus  ble = FlutterBluePlus();
 
+  Future scanDevices() async{
+    if(await Permission.bluetoothScan.request().isGranted) {
       await FlutterBluePlus.startScan(timeout: const Duration(seconds: 20),continuousUpdates: true);
 
       await Future.delayed(const Duration(seconds: 20), () {
@@ -40,21 +22,7 @@ class BleController extends GetxController{
         });
         FlutterBluePlus.stopScan();
       });
-      // print("hello world");
-      //FlutterBluePlus.stopScan();
     }
-
-    // Future.delayed(const Duration(seconds: 60), () {
-    //   FlutterBluePlus.stopScan();
-    // });
-  }
-  Future DisplayResult() async{
-    FlutterBluePlus.onScanResults.listen((results) {
-      print(results);
-      _observedScanResults.assignAll(results);
-    });
   }
   Stream<List<ScanResult>> get onScanResults => FlutterBluePlus.onScanResults;
-
 }
-
